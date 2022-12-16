3d7d9964c3b57344106661c5758a73e33e9c2282
hitendrasinh-Parmar <hitrajput098@gmail.com>
2022-12-16 21:05:02 +0530

    fix

--- src/navigation/home.navigator.tsx
+++ src/navigation/home.navigator.tsx
@@ -9,7 +9,6 @@ const HomeTabsNavigator = (): React.ReactElement => {
       screenOptions={{ headerShown: false }}
     >
       <TabsStack.Screen name={TabsRoutes.Profile} component={ProfileNavigator} />
-      <TabsStack.Screen name={TabsRoutes.Profile} component={ProfileNavigator} />
     </TabsStack.Navigator>
   );
 };
